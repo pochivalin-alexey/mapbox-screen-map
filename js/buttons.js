fc644bc508c26257661ee8d7cb73807c4379463c
Pochivalin Alexey <gorchok@mail.ru>
2020-07-06 09:30:05 +0300

    add custom mapbox gl screenControl

---
+++ js/buttons.js
@@ -0,0 +1,27 @@
+const screenControl = {
+  onAdd: (map) => {
+    this.map = map;
+    this.container = document.createElement("div");
+    this.container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+
+    const btn = document.createElement("button");
+    btn.className = "mapboxgl-ctrl-icon fa fa-image";
+    btn.title = title = "Screenshot";
+    btn.addEventListener(
+      "click",
+      (e) => {
+        toggleScreenWin();
+      },
+      false
+    );
+    this.container.appendChild(btn);
+    return this.container;
+  },
+  onRemove: () => {
+    this.container.parentNode.removeChild(this.container);
+    this.map = undefined;
+  },
+  getDefaultPosition: () => {
+    return "top-right";
+  },
+};
--- js/main.js
+++ js/main.js
@@ -12,32 +12,12 @@ const scale = new mapboxgl.ScaleControl({
   unit: "metric",
 });
 map.addControl(scale);
+map.addControl(screenControl);
 
 var draw = new MapboxDraw({
   displayControlsDefault: true,
   userProperties: true,
 });
-
-/********************************************** */
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-/********************************************** */
 
 map.on("style.load", async (e) => {
   map.addControl(draw);
@@ -46,11 +26,6 @@ map.on("style.load", async (e) => {
   map.getSource("line").setData(featureLine);
   map.getSource("fill").setData(featurePolygon);
   map.getSource("multi_point").setData(featureMultiPoint);
-});
-
-const screen = document.querySelector("#screen-btn");
-screen.addEventListener("click", () => {
-  toggleScreenWin();
 });
 
 function toggleScreenWin() {
